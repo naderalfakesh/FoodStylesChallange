6a438071b69d7d23a6f86309f4a1a70cb0726b47
Nader Alfakesh <nader.alfakesh@gmail.com>
2022-06-04 16:42:59 +0300

    Update status bar theme to match the design

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { CardListScreen } from './src/screens';
 import { client } from './src/services/graphql';
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <ApolloProvider client={client}>
       <Provider store={store}>
+        <StatusBar barStyle="light-content" translucent />
         {/* In real application screens will be inside the navigation container */}
         <CardListScreen />
       </Provider>
