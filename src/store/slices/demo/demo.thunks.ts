cb039851696798b88ceb621f71b55f7a5af6119d
Nader Alfakesh <nader.alfakesh@gmail.com>
2022-06-04 13:01:56 +0300

    Add redux store with demo example

---
+++ src/store/slices/demo/demo.thunks.ts
@@ -0,0 +1,21 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {demo} from './demo.slice';
+import {FetchAllParams} from './demo.types';
+
+export const fetchAll = createAsyncThunk(
+  'demo/fetchAll',
+  async (params: FetchAllParams, {getState, dispatch, rejectWithValue}) => {
+    try {
+      console.log(params);
+      const state = getState();
+      console.log(state);
+      const response = await new Promise<any>(resolve => {
+        resolve(null);
+      });
+      dispatch(demo);
+      return response;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  },
+);
---
+++ src/store/slices/demo/demo.types.ts
@@ -0,0 +1,9 @@
+export type InitialState = {
+  loading: boolean;
+  data?: any;
+  error?: any;
+};
+
+export type FetchAllParams = {
+  id: string;
+};
